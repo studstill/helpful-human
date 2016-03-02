3508498a8ec2dbf31f9a9ed88f95214a82468981
Jason Studstill <jason@jasonstudstill.com>
2016-03-02 15:10:31 -0800

    Move app.jsx and factor our the Main component

--- src/scripts/app.jsx
+++ src/scripts/app.jsx
@@ -2,15 +2,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class Main extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Got Here!</h1>
-      </div>
-    );
-  }
-}
+import Main from './components/Main.jsx';
 
 ReactDOM.render(<Main />, document.getElementById(`app`));
