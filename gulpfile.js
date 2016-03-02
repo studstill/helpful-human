99dbcf778e1d436077cafeb91ccc95821b1edbb0
Jason Studstill <jason@jasonstudstill.com>
2016-03-02 15:29:19 -0800

    Add htmlCopy and sass tasks; clean up webpack task

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@
 
 const gulp = require(`gulp`);
 const webpack = require(`gulp-webpack`);
-const PATH = __dirname;
+const sass = require(`gulp-sass`);
+const sourcemaps = require(`gulp-sourcemaps`);
+const autoprefixer = require(`gulp-autoprefixer`);
 
 gulp.task(`webpack`, ['copyHtml'], () => {
-  return gulp.src(`${PATH}/src/app.jsx`)
+  return gulp.src(`${__dirname}/src/scripts/app.jsx`)
     .pipe(webpack({
       watch: true,
       module: {
@@ -15,10 +17,29 @@ gulp.task(`webpack`, ['copyHtml'], () => {
         filename: `bundle.js`
       },
     }))
-    .pipe(gulp.dest(`public/`));
+    .pipe(gulp.dest(`public/scripts/`));
 });
 
-gulp.task('copyHtml', () => {
-  return gulp.src(`${PATH}/src/*.html`)
-    .pipe(gulp.dest(`${PATH}/public`))
+gulp.task('sass', function() {
+  return gulp.src(`${__dirname}/src/stylesheets/**/*.sass`)
+    .pipe(sass({
+      errLogToConsole: true,
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+    }))
+    .pipe(gulp.dest(`public/css/`));
 });
+
+// gulp.task('sass:watch', function() {
+//   return gulp.watch('./**/*.sass', ['sass']);
+// });
+
+gulp.task(`copyHtml`, () => {
+  return gulp.src(`${__dirname}/src/*.html`)
+    .pipe(gulp.dest(`${__dirname}/public`))
+});
+
+gulp.task(`default`, [`copyHtml`, `sass`, `webpack`]);
