383c668cfc4897c3bcc5f5613b2621250ec5a726
Jason Studstill <jason@jasonstudstill.com>
2016-03-06 23:24:58 -0800

    Properly style header except for hamburger icon

--- src/scripts/components/Header.jsx
+++ src/scripts/components/Header.jsx
@@ -12,44 +12,42 @@
      return (
        <div>
          <nav className="navbar navbar-default" role="navigation">
-           <div className="container">
-             <h1><a href="index.html">Create Class</a></h1>
-             <div className="navbar-header list-inline">
-               <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
-                 <span className="sr-only">Toggle navigation</span>
-                 <span className="icon-bar"></span>
-                 <span className="icon-bar"></span>
-                 <span className="icon-bar"></span>
-               </button>
-             </div>
-             <div className="collapse navbar-collapse" id="navbar-collapse-1">
-               <ul className="nav navbar-nav nav-tabs">
-                 <li>
-                   <a href="/details">
-                     Details
-                     <span className="glyphicon glyphicon-menu-right"></span>
-                   </a>
-                 </li>
-                 <li>
-                   <a href="/schedule">
-                     Schedule
-                     <span className="glyphicon glyphicon-menu-right"></span>
-                   </a>
-                 </li>
-                 <li>
-                   <a href="/payment">
-                     Payment
+           <h1><a href="/">Create a Class</a></h1>
+           <div className="navbar-header pull-left">
+             <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+               <span className="sr-only">Toggle navigation</span>
+               <span className="icon-bar"></span>
+               <span className="icon-bar"></span>
+               <span className="icon-bar"></span>
+             </button>
+           </div>
+           <div className="collapse navbar-collapse" id="navbar-collapse-1">
+             <ul className="nav navbar-nav nav-tabs">
+               <li>
+                 <a href="/details">
+                   Details
                    <span className="glyphicon glyphicon-menu-right"></span>
-                   </a>
-                 </li>
-                 <li>
-                   <a href="/confirm">
-                     Confirm
+                 </a>
+               </li>
+               <li>
+                 <a href="/schedule">
+                   Schedule
                    <span className="glyphicon glyphicon-menu-right"></span>
-                   </a>
-                 </li>
-               </ul>
-             </div>
+                 </a>
+               </li>
+               <li>
+                 <a href="/payment">
+                   Payment
+                 <span className="glyphicon glyphicon-menu-right"></span>
+                 </a>
+               </li>
+               <li>
+                 <a href="/confirm">
+                   Confirm
+                 <span className="glyphicon glyphicon-menu-right"></span>
+                 </a>
+               </li>
+             </ul>
            </div>
          </nav>
        </div>
